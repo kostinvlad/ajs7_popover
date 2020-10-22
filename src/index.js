604d184687c6d61482f50472c3809b8c67b86b88
Vladislav Kostin <v1adyank0stin@gmail.com>
2020-10-22 19:31:42 +0400

    some revisions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import './js/app.js'
 import "./css/style.css"
 
-document.getElementById('pop').addEventListener('click', (e) => {
-    e.preventDefault();
-    let set = e.currentTarget.closest('.set');
-    let button = e.currentTarget;
-    let popover = set.querySelector('.popover');
-    set.querySelector('.popover').classList.toggle('show')
-    set.querySelector('.popover').classList.toggle('hide')    
-    popover.style.left = `${button.offsetLeft - ((popover.offsetWidth - button.offsetWidth)/2)}px`
-    popover.style.top = `${button.offsetTop - button.offsetWidth / 2}px`    
-})
 
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+document.getElementById('pop').addEventListener('click', (e) => {
+    e.preventDefault();
+    let set = e.currentTarget.closest('.set');
+    let button = e.currentTarget;
+    let popover = set.querySelector('.popover');
+    set.querySelector('.popover').classList.toggle('show')
+    set.querySelector('.popover').classList.toggle('hide')    
+    popover.style.left = `${button.offsetLeft - ((popover.offsetWidth - button.offsetWidth)/2)}px`
+    popover.style.top = `${button.offsetTop - button.offsetWidth / 2}px`    
+})
